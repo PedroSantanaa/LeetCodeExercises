3040af92c58d60d7152909f6f49b305c3dabc7aa
pedro santana <pedro.ferraz01@gmail.com>
2023-10-20 10:22:35 -0300

    Finish Day 1

---
+++ hackerrank/Day 1/Mini-Max Sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+  /*
+   * Complete the 'miniMaxSum' function below.
+   *
+   * The function accepts INTEGER_ARRAY arr as parameter.
+   */
+
+  public static void miniMaxSum(List<long> arr)
+  {
+    arr.Sort();
+    List<long> arrMax = new List<long>(arr);
+    List<long> arrMin = new List<long>(arr);
+    arrMax.RemoveAt(0);
+    arrMin.RemoveAt(arrMin.Count - 1);
+    long sumMax = arrMax.Sum();
+    long sumMin = arrMin.Sum();
+    Console.Write(sumMin + " " + sumMax);
+  }
+
+}
+
+class Solution
+{
+  public static void Main(string[] args)
+  {
+
+    List<long> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+
+    Result.miniMaxSum(arr);
+  }
+}
---
+++ hackerrank/Day 1/Time Conversion/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+  /*
+   * Complete the 'timeConversion' function below.
+   *
+   * The function is expected to return a STRING.
+   * The function accepts STRING s as parameter.
+   */
+
+  public static string timeConversion(string s)
+  {
+    List<string> time = s.Split(':').ToList();
+    string hour = time[0];
+    string minutes = time[1];
+    string seconds = time[2].Substring(0, 2);
+    if (s.Contains("PM"))
+    {
+      if (hour != "12")
+      {
+        hour = (int.Parse(hour) + 12).ToString();
+      }
+    }
+    else
+    {
+      if (hour == "12")
+      {
+        hour = "00";
+      }
+    }
+    string result = hour + ":" + minutes + ":" + seconds;
+    return result;
+  }
+
+}
+
+
+class Solution
+{
+  public static void Main(string[] args)
+  {
+    TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+    string s = Console.ReadLine();
+
+    string result = Result.timeConversion(s);
+
+    textWriter.WriteLine(result);
+
+    textWriter.Flush();
+    textWriter.Close();
+  }
+}
