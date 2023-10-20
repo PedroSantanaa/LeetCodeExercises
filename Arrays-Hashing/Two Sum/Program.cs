950bc54b485f1c4c7b16e22ed8384af587fe6c7a
pedro santana <pedro.ferraz01@gmail.com>
2023-10-20 20:58:54 -0300

    leetcode question

---
+++ Arrays-Hashing/Two Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+  public int[] TwoSum(int[] nums, int target)
+  {
+    Dictionary<int, int> indices = new Dictionary<int, int>();
+    for (int i = 0; i < nums.Count; i++)
+    {
+      var diff = target - nums[i];
+      if (nums.Contains(diff) && Array.IndexOf(nums, diff) != i)
+      {
+        return new int[] { i, Array.IndexOf(nums, diff) };
+      }
+
+    }
+  }
+}
